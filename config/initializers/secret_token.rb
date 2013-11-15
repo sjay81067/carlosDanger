459501e20a1621bbb663b2415877390f4d8cc22f
Eric <hamilton.299@osu.edu>
2013-11-15 16:05:49 -0500

    sssss

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eval::Application.config.secret_token = '2544b8050dd4474d8421e613b139c0b9461fdbe74b1c4b5b742f4262bc74b7717a21e7eaec2b5e90ce98a04d19b010df386f6893ed9a48b3e852b84f99b8f8ac'
